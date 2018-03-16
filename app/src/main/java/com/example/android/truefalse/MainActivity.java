f425982a7f128371475b7d79e78ac1e844009198
Edson Hutton <SUPATURBOLOCITY@users.noreply.github.com>
2018-03-15 22:13:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/truefalse/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.truefalse;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.truefalse.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStartButton = (Button)findViewById(R.id.startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+    }
+}}
